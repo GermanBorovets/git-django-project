7935ff347f115cedfdd53a7f83c905beefd94297
German Borovets <germanborovets@icloud.com>
2021-04-17 20:36:29 +0300

    Diploy

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1fx#37plcj@i)3siul6^*(5r$!(ohxd_jclcbo=wlbabej_)1s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
