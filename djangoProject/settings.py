486cf67be5212245c22321bf2684e57207c24204
German Borovets <germanborovets@icloud.com>
2021-04-17 23:25:51 +0300

    Diploy

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,13 +76,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'd48kl9escape6o',
-        'HOST': 'ec2-54-161-239-198.compute-1.amazonaws.com',
-        'PORT': 5432,
-        'USER': 'muglqpebzrzoma',
-        'PASSWORD': '4d68f650804ebfb352fcf440e63b31e7d73f1bd61062730d761e19c50f820585',
     }
 }
 
