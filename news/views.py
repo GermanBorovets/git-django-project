f4e98ca5367b1f47a85fd7b141724305dbb4b974
German Borovets <germanborovets@icloud.com>
2021-04-17 18:43:25 +0300

    GIT init

---
+++ news/views.py
@@ -0,0 +1,119 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from .models import News, Category
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from .utils import MyMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегестрировались!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка Регистрации!')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'news/register.html', {"form": form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'news/login.html', {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    # extra_context = {'title': 'Главная'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+
+
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+
+class ViewNews(DetailView):
+    model = News
+    # pk_url_kwarg = 'news_id'
+    # template_name = 'news/news_detail.html'
+    context_object_name = 'news_item'
+
+
+class CreateNews(LoginRequiredMixin, CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+    login_url = '/admin/'
+
+# def index(request):
+#    news = News.objects.order_by('-created_at')
+#    return render(request, 'news/index.html', {'news': news, 'title': 'Список Новостей',# })
+
+
+# def get_category(request, category_id):
+#   news = News.objects.filter(category_id=category_id)
+#    category = Category.objects.get(pk=category_id)
+#    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+# def view_news(request, news_id):
+#    # news_item = News.objects.get(pk=news_id)
+#    news_item = get_object_or_404(News, pk=news_id)
+#    return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+# def add_news(request):
+#    if request.method == 'POST':
+#        form = NewsForm(request.POST)
+#        if form.is_valid():
+#            news = form.save()
+#            return redirect(news)
+#    else:
+#        form = NewsForm()
+#    return render(request, 'news/add_news.html', {'form': form})
